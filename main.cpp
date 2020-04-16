7b136c7844585f287a3d5a437c0ef14211d8694f
ko040123 <21600124@handong.edu>
2020-04-16 02:45:54 -0700

    Sorting Program

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include "sorting.h"
 
+using namespace std;
+
 int sorted[10];
+int arr[10];
 
+int N = 10;
 int main(){
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+    
+    merge_sort(arr, 0, N-1);
 
+    for(int i = 0; i < N; i++){
+        cout << sorted[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
